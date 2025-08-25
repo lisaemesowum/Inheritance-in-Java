713693f79e02b606dfbba6deb99b6a5e0a6c3166
Lisa Emesowum Amarachi <lisaemesowum8@gmail.com>
2025-08-26 00:48:56 +0100

    Update Student.java

--- src/main/java/inheritance/Student.java
+++ src/main/java/inheritance/Student.java
@@ -1,4 +1,16 @@
 package inheritance;
 
-public class Student {
+public class Student extends Person {
+    char Grade;
+    int rollback;   //unique identifier for a student within a class or school.
+     // create a constructor connecting from the parent
+
+    public Student(String name, int age, String address, String gender , char Grade, int rollback) {
+        super(name, age, address, Gender.valueOf(gender));
+        this.Grade = Grade;
+        this.rollback = rollback;
+    }
+    public String  displayStudentInfo(){
+        return super.displayInfo() + "\nGrades: " + Grade + "\n rollbacks: " + rollback;
+    }
 }
