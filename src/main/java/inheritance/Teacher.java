d47c20b54cce7de897ba67b8748a94172694561a
Lisa Emesowum Amarachi <lisaemesowum8@gmail.com>
2025-08-26 00:48:23 +0100

    Update Teacher.java

--- src/main/java/inheritance/Teacher.java
+++ src/main/java/inheritance/Teacher.java
@@ -1,4 +1,20 @@
 package inheritance;
 
-public class Teacher {
+public class Teacher extends Person {
+    String subject;
+    double salary;
+
+    // constructor
+    public Teacher(String name, int age, String address, String gender , String subject, double salary){
+        super(name, age, address, Gender.valueOf(gender));
+        this.subject = subject;
+        this.salary = salary;
+    }
+    public String displayTeacherInfo(){
+        return super.displayInfo() + "\nSubjects: " + subject + "\nTeachers Salary: " +salary;
+    }
+    public double calculateSalary(double bonus){
+        return salary + bonus;
+
+    }
 }
